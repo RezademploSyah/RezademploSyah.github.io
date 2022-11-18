67ae2bea5e993ea728b3a2bdc78a8d3441455350
RezademploSyah <reza_syah_2022@abbs.alabidin.sch.id>
2022-11-18 17:29:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//jquery script
+
+$(document).ready(function(){
+
+    //menekan tombol (button) click me
+    $("button").click(function(){
+        //menghilangkan element paragraph (p) dengan menggunakan action hidde ()
+        $("p").toggle();
+    });
+});
---
+++ tilticoba.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.cobajs').tilt({
+    scale: 1.2,
+      glare: true,
+      maxGlare: .5,
+  })
+
+  $('.sm-tilt').tilt({
+    scale: 0.9,
+      glare: true,
+      maxGlare: .9
+  
+  })
+  
+})
